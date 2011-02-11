f15f90872237273b932ec28bf587b0f81af24642
Leif Tore Rusten <rusten@stud.ntnu.no>
2011-02-11 15:55:00 +0100

    Only sandboxing git-branch.

--- prefetcher/prefetcher.cc
+++ prefetcher/prefetcher.cc
@@ -12,7 +12,7 @@ void prefetch_init(void)
     /* Called before any calls to prefetch_access. */
     /* This is the place to initialize data structures. */
 
-    DPRINTF(HWPrefetch, "Initialized sequential-on-access prefetcher\n");
+    DPRINTF(HWPrefetch, "Initialized sequential-on-happy prefetcher\n");
 }
 
 void prefetch_access(AccessStat stat)
