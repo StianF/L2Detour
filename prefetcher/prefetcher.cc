3f2e4d22ad4444bc37cec6ed3b7be93cfd4ed98f
Leif Tore Rusten <rusten@stud.ntnu.no>
2011-03-08 18:30:07 +0100

    Created first suggestion for the data structures needed for tree version.

--- prefetcher/prefetcher.cc
+++ prefetcher/prefetcher.cc
@@ -14,31 +14,54 @@ void prefetch_init(void)
 }
 
 typedef struct node {
-  Addr pc;
+  Addr delta;
+  int count;
   node_t *child;
   node_t *sibling;
 } node_t;
 
+typedef struct pf {
+  node_t *node;
+  int count;
+  char learning;
+} pf_t;
+
+typedef struct root {
+  Addr pc;
+  node_t *cur;
+  node_t *child;
+  pf_t pf;
+} root_t;
+
 #define NUM_ROOTS 100
-node_t *roots[NUM_ROOTS + 1];
+root_t roots[NUM_ROOTS + 1];
 roots[NUM_ROOTS] = 0;
+int rr_count = 0;
 
 void prefetch_access(AccessStat stat) {
   // Search for matching root.
   node_t *root = roots;
   while (root) {
-    if (root->pc != stat.pc) break; // Found a match.
+    if (root->pc == stat.pc) break; // Found a match.
     root++;
   }
 
   // Didn't find a match, add one.
   if (!root) {
-    // root is actually now an empty slot. If not memguard, use it.
+    // root is actually now an empty slot. If not guard value, use it.
+    // If guard value need to choose a slot in use.
     if (root == roots[NUM_ROOTS]) {
-      // Throw out the LRU?
-    } else {
-      // Use the empty slot
+      // TODO: Throw out according to a suitable replacement policy (LRU?).
+      // For now just use round-robin.
+      root = roots[rr_count++];
+      if (rr_count == NUM_ROOTS) rr_count = 0;
     }
+
+    root->child = malloc(sizeof(node));
+    root->child->delta = 1;
+    root->child->count = 4;
+    root->pf->node = root->child;
+    root->pf->learning = 1; // TRUE
   }
 }
 
