5b0219cb38c622a295107005bdd09f6407dd3645
Leif Tore Rusten <rusten@stud.ntnu.no>
2011-03-30 08:29:03 +0200

    Modified the scripts to not be dependent on a specific file name. Still has some non-general buggy stuff though.

--- prefetcher/juggle_numbers.sh
+++ prefetcher/juggle_numbers.sh
@@ -1,4 +1,3 @@
-cat N\ for\ speedup | \
   awk 'BEGIN { n = 0; last = 0; print "S,\tN\tSpeedup\tDiff" }; \
   { if (n == 0) denom = $3; \
   print $1 "\t" $2 "\t" $3 / denom "\t" $3 / denom - last; \
