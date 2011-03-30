1dfc00ae4d59c189a11e48d2f6feb6523646e71f
Leif Tore Rusten <rusten@stud.ntnu.no>
2011-03-30 18:14:04 +0200

    Added the sequential degree that gave best speedup locally.

--- prefetcher/prefetcher.cc
+++ prefetcher/prefetcher.cc
@@ -5,17 +5,66 @@
 */
 
 #include "interface.hh"
+
 struct RPT {
 	Addr pc;        
 	Addr last_mem_addr; 
-	uint16_t* deltas;
-	int deltai;
-	int fill;
+	short* deltas;
+	unsigned char deltai;
+	unsigned char fill;
 	Addr last_prefetch;
 };
-const int l = 110;
-const int deltal = 10;
+const int l = 120;
+const int deltal = 16;
 RPT rpttable[l];
+
+Addr cachetable[MAX_QUEUE_SIZE];
+int cachefill = 0;
+
+bool in_cache_queue(Addr a){
+	Addr block = a/BLOCK_SIZE;
+	int i = 0;
+	for(i = 0; i < cachefill; i++){
+		if(block == cachetable[i]){
+			return true;
+		}
+	}
+	return false;
+}
+
+void insert_cache_queue(Addr a){
+		Addr block = a/BLOCK_SIZE;
+		if(cachefill != 0){
+			int i = 0;
+			for(i = cachefill-1; i > 0; i--){
+				cachetable[i] = cachetable[i-1];
+			}
+		}
+		cachetable[0] = block; 
+		if(cachefill != MAX_QUEUE_SIZE){
+			cachefill++;
+		}
+
+}
+
+void remove_from_cache_queue(Addr a){
+	Addr block = a/BLOCK_SIZE;
+	int i = 0;
+	for(i = 0; i < cachefill; i++){
+		if(cachetable[i] == block){
+			int j = 0;
+			for(j = i+1; j < cachefill; j++){
+				cachetable[i] = cachetable[j];
+			}
+			cachefill--;
+			break;
+		}
+	}
+}
+
+bool canPrefetch(Addr a){
+	return (a <= MAX_PHYS_MEM_ADDR/* && !get_prefetch_bit(a)*/ && !in_mshr_queue(a) && !in_cache(a) && !in_cache_queue(a));
+}
 
 Addr* DC(int i);
 void PF(int i, Addr* cand);
@@ -35,12 +84,14 @@ void prefetch_access(AccessStat stat)
 {
 	bool found = false;
 	int foundi = 0;
+	begindelete = false;
+	n_cand = 0;
 	int i = 0;
 	for(i = 0; i < far; i++){
 		if(rpttable[i].pc == stat.pc){
 			found = true;
 			foundi = i;
-			if(stat.mem_addr - rpttable[i].last_mem_addr != 0){
+			if(stat.mem_addr - rpttable[i].last_mem_addr != 0 ){
 				if(rpttable[i].deltai < deltal-1){
 					rpttable[i].deltas[rpttable[i].deltai++] = stat.mem_addr - rpttable[i].last_mem_addr;
 					if(rpttable[i].fill != deltal-1){
@@ -51,8 +102,12 @@ void prefetch_access(AccessStat stat)
 					rpttable[i].deltai = 0;
 					rpttable[i].fill = deltal-1;
 				}
-				rpttable[i].last_mem_addr = stat.mem_addr;
 			}
+			if(((stat.mem_addr - rpttable[i].last_mem_addr) > 32768)){//Fucker seg med det her|| ((stat.mem_addr - rpttable[i].last_mem_addr) < (-65536))){
+				rpttable[i].deltai = 0;
+				rpttable[i].fill = 0;
+			}
+			rpttable[i].last_mem_addr = stat.mem_addr;
 		}
 	}
 	if(!found){
@@ -65,7 +120,7 @@ void prefetch_access(AccessStat stat)
 			length = 0;
 		}
 		if(begindelete){
-			uint16_t* deltas = rpttable[length].deltas;
+			short* deltas = rpttable[length].deltas;
 			delete[] deltas;
 		}
 		RPT rpt;
@@ -74,22 +129,36 @@ void prefetch_access(AccessStat stat)
 		rpt.fill = 0;
 		rpt.last_mem_addr = stat.mem_addr;
 		rpt.last_prefetch = 0;
-		rpt.deltas = new uint16_t[deltal];
+		rpt.deltas = new short[deltal];
 		rpttable[length] = rpt;
 	}else if(rpttable[foundi].fill > 2) {
 		Addr* candidates = DC(foundi);
 		PF(foundi, candidates);
 		delete[] candidates;
 	}
+        // Sequential failover.
+	if(n_cand == 0){
+                Addr pref = stat.mem_addr;
+                int n;
+                for (n = 0; n < 4; n++) {
+                  pref += BLOCK_SIZE;
+                  if(canPrefetch(pref)){
+			issue_prefetch(pref);
+			insert_cache_queue(pref);
+//			set_prefetch_bit(pref);
+                  }
+                }
+	}
 }
+
 
 Addr* DC(int i){
 	Addr* candidates = new Addr[deltal];
 	int patterns[deltal];
 	int n_patterns = 0;
 	n_cand = 0;
-	uint16_t d1 = rpttable[i].deltas[rpttable[i].deltai];
-	uint16_t d2 = 0;
+	short d1 = rpttable[i].deltas[rpttable[i].deltai];
+	short d2 = 0;
 	int delta = 0;
 	if(rpttable[i].deltai == 0){
 		delta = deltal-1;
@@ -122,7 +191,7 @@ Addr* DC(int i){
 		}
 	}
 	int j = 0;
-	for(j = 0; j < n_patterns; j++){
+	//for(j = 0; j < n_patterns; j++){
 		Addr addr = rpttable[i].last_mem_addr;
 		int w = patterns[j];
 		int next = (j+1 < n_patterns)?patterns[j+1]:patterns[0];
@@ -135,7 +204,7 @@ Addr* DC(int i){
 					int x = 0;
 					bool found = false;
 					for(x = 0; x < n_cand; x++){
-						if(candidates[x] == addr){
+						if(candidates[x] == addr || candidates[x]/BLOCK_SIZE == addr/BLOCK_SIZE){
 							found = true;
 						}
 					}
@@ -153,7 +222,7 @@ Addr* DC(int i){
 				w = 0;
 			}
 		}
-	}
+	//}
 	return candidates;
 }
 
@@ -162,15 +231,21 @@ void PF(int i, Addr* cand){
 
 	int j = 0;
 	for(j = 0; j < n_cand; j++){
-		if(!in_cache(cand[j]) && !in_mshr_queue(cand[j]) && cand[j] <= MAX_PHYS_MEM_ADDR && cand[j] != rpttable[i].last_prefetch && current_queue_size() < MAX_QUEUE_SIZE){
+		if(canPrefetch(cand[j]) && cand[j] != rpttable[i].last_prefetch){
 			issue_prefetch(cand[j]);
 			rpttable[i].last_prefetch = cand[j];
+			insert_cache_queue(cand[j]);
+			//set_prefetch_bit(cand[j]);
 		}
 	}
 }
 
 void prefetch_complete(Addr addr) {
+//	clear_prefetch_bit(addr);
+	remove_from_cache_queue(addr);
 	/*
 	 * Called when a block requested by the prefetcher has been loaded.
 	 */
 }
+
+
